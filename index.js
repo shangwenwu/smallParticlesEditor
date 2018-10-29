64716da2c8f1fead33dd2307fe5defcebfcac4fc
happysww1230 <happysww1230@163.com>
2018-10-29 17:22:20 +0800

    init

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
-
-exports.dd = {
-  aa:111,
-  cc: function (arg) {
-    this.aa = arg;
-    console.log(arg)
+; (function (root, factory) {
+  if (typeof exports === "object") {
+    // CommonJS
+    module.exports = exports = factory();
   }
-};
+  else if (typeof define === "function" && define.amd) {
+    // AMD
+    define([], factory);
+  }
+  else {
+    // Global (browser)
+    root.Editor = factory();
+  }
+}(this, function () {
+
+  var Editor = {
+    aa: 11,
+    bb: 22,
+    cc: function(arg){
+      this.aa = arg;
+      console.log(arg)
+    }
+  }
+  return Editor;
+
+}));
